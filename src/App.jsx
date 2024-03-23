7f41052a2b5b773d87b4428ca12878faf12fed85
Mikiqueen0 <mikiqueen2004@gmail.com>
2024-03-23 20:51:53 +0700

    SignUp & Login UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { 
+//   SignUp,
+//   Login 
+// } from './pages'
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
+const App = () => (
+  <>
+    {/* <SignUp /> */}
+    <Login/>
+  </>
+);
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        poppin: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        'primary': "#262626",
+        "emerald-green": "#00BE85",
+        "darkest-black": "#0F0F0F",
+        "dark-field": "#111111",
+        "field": "#181818"
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      screens: {
+        "wide": "1440px"
+      }
+    },
+  },
+  plugins: []
+}
+
