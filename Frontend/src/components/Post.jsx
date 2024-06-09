c1ab8d534b72094e35f5a5d3ae5107cc756ecc83
martksk <krtsk2547@gmail.com>
2024-06-09 23:12:11 +0700

    display image

--- Frontend/src/components/Post.jsx
+++ Frontend/src/components/Post.jsx
@@ -11,7 +11,7 @@ export default function Post({ post }) {
   const postCreator = post.userID;
   const postCommunity = post.communityID;
   const { postID } = useParams();
-  
+
   const handleClickPost = (navigateToPostDetail) => {
     if (navigateToPostDetail) {
       navigate(`/post/${post._id}`);
@@ -23,75 +23,96 @@ export default function Post({ post }) {
 
   return (
     <div>
-    <div className={`bg-primary rounded-[10px] px-5 py-4 max-h-[800px] ${postID ? "cursor-default" : "cursor-pointer"}`} onClick={() => handleClickPost(true)}>
-      <div className="flex justify-between max-h-[500px]">
-        <div className="flex flex-row items-center gap-2">
-          <img
-            src={postCommunity?.image}
-            alt="profile"
-            className="rounded-full object-cover size-[1.75rem]"
-          />
-          <p
-            className="font-normal text-[15px] text-opacity-90 text-white hover:underline cursor-pointer"
-            onClick={() => navigate(`/community/${postCommunity?._id}`)}
-          >
-            {postCommunity?.name}
-          </p>
-          <p className="font-normal text-[12px] text-opacity-60 text-white">
-            Posted by{" "}
-            <span
-              className="hover:underline cursor-pointer"
-              onClick={() => navigate(`/profile/${postCreator?.username}`)}
+      <div
+        className={`bg-primary rounded-[10px] px-5 py-4 max-h-[1000px] ${
+          postID ? "cursor-default" : "cursor-pointer"
+        } relative`}
+        onClick={() => handleClickPost(true)}
+      >
+        <div className="flex justify-between max-h-[500px]">
+          <div className="flex flex-row items-center gap-2">
+            <img
+              src={postCommunity?.image}
+              alt="profile"
+              className="rounded-full object-cover size-[1.75rem]"
+            />
+            <p
+              className="font-normal text-[15px] text-opacity-90 text-white hover:underline cursor-pointer"
+              onClick={() => navigate(`/community/${postCommunity?._id}`)}
             >
-              {postCreator?.username}
-            </span>
-            <span> - </span>
-            <span className="text-opacity-60 text-xs">
-              {FormatTime(post?.createdAt)}
-            </span>
+              {postCommunity?.name}
+            </p>
+            <p className="font-normal text-[12px] text-opacity-60 text-white">
+              Posted by{" "}
+              <span
+                className="hover:underline cursor-pointer"
+                onClick={() => navigate(`/profile/${postCreator?.username}`)}
+              >
+                {postCreator?.username}
+              </span>
+              <span> - </span>
+              <span className="text-opacity-60 text-xs">
+                {FormatTime(post?.createdAt)}
+              </span>
+            </p>
+          </div>
+
+          <EllipsisHorizontalIcon className="text-white size-6" />
+        </div>
+        <div className="flex gap-2 pt-3">
+          {Array.isArray(post.tag) &&
+            post.tag.map((tag, key) => {
+              return (
+                <button
+                  key={key}
+                  className="text-[12px] font-medium text-opacity-90 text-emerald-green rounded-[10px] border border-emerald-green px-3 py-2"
+                >
+                  {tag}
+                </button>
+              );
+            })}
+        </div>
+        <div className="py-3">
+          <p className="text-[14px] font-normal text-opacity-[78%] text-white">
+            {post.desc}
           </p>
         </div>
-
-        <EllipsisHorizontalIcon className="text-white size-6" />
-      </div>
-      <div className="flex gap-2 pt-3">
-        {Array.isArray(post.tag) && post.tag.map((tag, key) => {
-          return (
-            <button
-              key={key}
-              className="text-[12px] font-medium text-opacity-90 text-emerald-green rounded-[10px] border border-emerald-green px-3 py-2"
+        <div
+          className={`grid grid-rows-1 ${
+            post.image.length > 1 ? "grid-cols-2" : "grid-cols-1"
+          } grid-flow-row`}
+        >
+          {post.image.map((imageUrl, index) => (
+            <div
+              key={index}
+              className={`${
+                index === 2 && post.image.length !== 4 ? "col-span-2" : ""
+              }`}
             >
-              {tag}
-            </button>
-          );
-        })}
+              <img
+                src={imageUrl}
+                alt=""
+                className={`cursor-pointer w-full h-full object-cover ${
+                  post.image.length > 1 ? "max-h-[300px]" : "max-h-[1000px]"
+                }`}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-row items-center justify-around gap-8 text-[14px] font-medium text-opacity-90 text-white mt-3 hover:emerald-green">
+          <button>Like</button>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              handleClickPost(false);
+            }}
+          >
+            Comment
+          </button>
+          <button>Save</button>
+        </div>
       </div>
-      <div className="pt-3">
-        <p className="text-[14px] font-normal text-opacity-[78%] text-white">
-          {post?.desc}
-        </p>
-      </div>
-      <div
-        className={`gap-4 rounded-[10px] pt-3 grid ${
-          post.image?.length > 1 ? "grid-cols-2" : "grid-cols-1"
-        }`}
-      >
-        {post.image?.map((imageUrl, index) => (
-          <img
-            key={index}
-            src={imageUrl}
-            alt=""
-            className="rounded-[10px] object-cover w-full h-full cursor-pointer] max-h-[500px]"
-          />
-        ))}
-      </div>
-      <div className="flex gap-8 text-[14px] font-medium text-opacity-90 text-white mt-3">
-        <button>Like</button>
-        <button onClick={(e) => {e.stopPropagation(); handleClickPost(false);}}>Comment</button>
-        <button>Save</button>
-      </div>
-    </div>
-    {postID && <CommentSection postID={post._id}/>}
+      {postID && <CommentSection postID={post._id} />}
     </div>
   );
 }
